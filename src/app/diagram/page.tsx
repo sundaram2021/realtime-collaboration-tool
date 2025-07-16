53ec82a76329f630e7d461ecb79c95d4a0a48592
Sundaram Kumar Jha <jhasundaram@outlook.com>
2025-07-16 15:05:36 +0530

    fix: props

--- src/app/diagram/page.tsx
+++ src/app/diagram/page.tsx
@@ -2,12 +2,12 @@ import { Suspense } from 'react';
 import Loading from '@/components/loading';
 import DiagramContent from '@/components/diagram/diagram-content';
 
-type DiagramPageProps = {
-    params: { [key: string]: string };
+export default function DiagramPage({
+    searchParams,
+}: {
+    params: { [key: string]: string | string[] | undefined };
     searchParams: { [key: string]: string | string[] | undefined };
-};
-
-export default function DiagramPage({ searchParams }: DiagramPageProps) {
+}) {
     const diagramId = searchParams.id as string;
     const permission = searchParams.permission as 'view' | 'edit';
 

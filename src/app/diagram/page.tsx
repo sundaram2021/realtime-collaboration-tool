3ab9c835dbfa5b1fd3c30dd43cef91728959168f
Sundaram Kumar Jha <jhasundaram@outlook.com>
2025-07-16 15:02:13 +0530

    fix: props

--- src/app/diagram/page.tsx
+++ src/app/diagram/page.tsx
@@ -1,16 +1,16 @@
 import { Suspense } from 'react';
 import Loading from '@/components/loading';
-import DiagramContent from '@/components/diagram/diagram-content'; // We will create this component next
+import DiagramContent from '@/components/diagram/diagram-content';
 
-export default function DiagramPage({
-    searchParams,
-}: {
+type DiagramPageProps = {
+    params: { [key: string]: string };
     searchParams: { [key: string]: string | string[] | undefined };
-}) {
+};
+
+export default function DiagramPage({ searchParams }: DiagramPageProps) {
     const diagramId = searchParams.id as string;
     const permission = searchParams.permission as 'view' | 'edit';
 
-    
     return (
         <Suspense fallback={<Loading />}>
             <DiagramContent diagramId={diagramId} permission={permission} />
